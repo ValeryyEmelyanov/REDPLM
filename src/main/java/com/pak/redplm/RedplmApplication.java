ef8356663ad59e69e6c84654288970c6c90c772f
Valery Emelyanov <westbrookokc1997@gmail.com>
2024-05-26 13:38:32 +0300

    0:   Add project

---
+++ src/main/java/com/pak/redplm/RedplmApplication.java
@@ -0,0 +1,13 @@
+package com.pak.redplm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedplmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedplmApplication.class, args);
+    }
+
+}
